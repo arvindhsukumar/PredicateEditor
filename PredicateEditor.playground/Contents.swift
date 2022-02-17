249eb8d511f383197cb5d691ee164f673c6a7ff2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-17 16:14:32 +0800

    Delete PredicateEditor.playground directory

--- PredicateEditor.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
-let vc = UIViewController()
-vc.view.backgroundColor = UIColor.blueColor()
-vc.view.frame
